73c55c893692693c706dca8b76a2778adf9ecb4f
Mateusz Król <matek8952@gmail.com>
2019-11-07 21:51:35 +0100

    Add model

--- mnist_clothing.py
+++ mnist_clothing.py
@@ -13,19 +13,45 @@ test_images = test_images / 255.0
 class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
-plt.figure()
-plt.imshow(train_images[0])
-plt.colorbar()
-plt.grid(False)
-plt.show()
+# Wyświetlanie zdjęcia
+# plt.figure()
+# plt.imshow(train_images[0])
+# plt.colorbar()
+# plt.grid(False)
+# plt.show()
 
 # Wyświetlenie 25-ciu pierwszych zdjęć ze zbioru uczącego
-plt.figure(figsize=(10,10))
-for i in range(25):
-    plt.subplot(5,5,i+1)
-    plt.xticks([])
-    plt.yticks([])
-    plt.grid(False)
-    plt.imshow(train_images[i], cmap=plt.cm.binary)
-    plt.xlabel(class_names[train_labels[i]])
-plt.show()
+# plt.figure(figsize=(10, 10))
+# for i in range(25):
+#     plt.subplot(5, 5, i+1)
+#     plt.xticks([])
+#     plt.yticks([])
+#     plt.grid(False)
+#     plt.imshow(train_images[i], cmap=plt.cm.binary)
+#     plt.xlabel(class_names[train_labels[i]])
+# plt.show()
+
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),
+    keras.layers.Dense(128, activation='sigmoid'),
+    keras.layers.Dense(10, activation='softmax')
+])
+
+model.compile(optimizer='nadam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(train_images, train_labels, epochs=10)
+
+test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
+print('\nTest accuracy:', test_acc)
+
+predictions = model.predict(test_images)
+
+# Prezentowanie wyniku
+# for i in range(5):
+#     plt.grid(False)
+#     plt.imshow(test_images[i], cmap=plt.cm.binary)
+#     plt.xlabel('Actual: ' + class_names[test_labels[i]])
+#     plt.title('Prediction: ' + class_names[np.argmax(predictions[i])])
+#     plt.show()
